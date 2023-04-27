1bd0799c346966cb2ad1978741cd4e1cb5153ea7
ekobdy <2207101079@student.stmik-aub.ac.id>
2023-04-27 22:58:34 +0700

    Upload Latihan 3

---
+++ anak_dari_anakClass.php
@@ -0,0 +1,59 @@
+<?php
+
+class Mahasiswa {
+
+	public string $nim;
+	public string $nama;
+
+	public function setNim(string $a) {
+		$this->nim = $a;
+	}
+
+	public function setNama(string $b) {
+		$this->nama = $b;
+	}
+}
+
+class Krs extends Mahasiswa {
+
+	protected stirng $matakuliah;
+	
+	public function getNim(){
+		return $this ->nim;
+	}
+
+	public function getNama(){
+		return $this ->nama;
+	}
+
+	public function setMatakuliah(string $c){
+		$this ->matakuliah = $c;
+	}
+}
+
+class Nilai extends Krs {
+
+	private static int $nilai=90;
+	
+	public function getMatakuliah(){
+		return $this->matakuliah;
+	}
+
+	public static function getNilai(){
+		return self::$nilai;
+	}
+}
+
+$nilai = new Nilai();
+
+//Memanggil class Mahasiswa
+$nilai -> setNim("2207101079");
+$nilai -> setNama("Eko Budiyanto");
+
+//Memanggil class Krs
+$nilai -> setMatakuliah("Pemrograman Berbasis Web Lanjutan");
+
+echo "<p>NIM = ".$nilai->getNim()."</p>";
+echo "<p>Nama = ".$nilai->getNama()."</p>";
+echo "<p>Matakuliah = ".$nilai->getMatakuliah()."</p>";
+echo "<p>Nilai = ".Nilai::getNilai(90)."</p>";
---
+++ overriding.php
@@ -0,0 +1,18 @@
+<?php
+
+class Mahasiswa {
+	public string $nim = "2207101079";
+	public function setNim(){
+		return $this ->nim;
+	}
+}
+
+class Krs extends Mahasiswa{
+	//Method override
+	public function setNim(){
+		return null;
+	}
+}
+
+$krs = new Krs();
+echo $krs->setNim();
